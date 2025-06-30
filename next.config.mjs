f8616c7a84ba62a613dffe1e1b04620518ce5fe4
notaigbe <notaigbe@gmail.com>
2025-06-30 20:56:50 +0100

    testing render deploy

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
     domains: ['via.placeholder.com'],
   },
+  output: "export",
+  // optionally:
+  trailingSlash: true, // recommended for static hosting
 };
 
 export default nextConfig;
